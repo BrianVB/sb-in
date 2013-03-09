86cb950d7873206e9b1a95709a4bc0d0ad9bf944
Brian Van Buren <brian@spacebrews.com>
2013-03-08 19:51:59 -0500

    enable gii, URL rewrites, connect to db

--- protected/config/main.php
+++ protected/config/main.php
@@ -20,14 +20,12 @@ return array(
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'hannah18',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			'ipFilters'=>array('72.230.180.24','::1'),
 		),
-		*/
 	),
 
 	// application components
@@ -37,29 +35,30 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+			'showScriptName'=>false,
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
+		/*
 		'db'=>array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
-		// uncomment the following to use a MySQL database
-		/*
-		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
-			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
-			'charset' => 'utf8',
-		),
 		*/
+		// uncomment the following to use a MySQL database
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=spacebrews_internal',
+			'emulatePrepare' => true,
+			'username' => 'sb_in_dbu',
+			'password' => '4methy$t1618',
+			'charset' => 'utf8',
+			'enableParamLogging'=>true,
+			'enableProfiling'=>true,			
+		),
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
@@ -85,6 +84,6 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'brian@spacebrews.com',
 	),
 );
