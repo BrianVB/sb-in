85f65e99419d1b39e07a50d187adaf9a31de9dd2
Brian Van Buren <brian@spacebrews.com>
2013-03-19 23:29:54 -0400

    Disallow access for guests

--- protected/controllers/UserController.php
+++ protected/controllers/UserController.php
@@ -27,12 +27,7 @@ class UserController extends Controller
 	public function accessRules()
 	{
 		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('create'),
-				'users'=>array('*'),
-			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('admin','delete','update'),
 				'users'=>array('@'),
 			),
 			array('deny',  // deny all users
