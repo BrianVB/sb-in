aa3ed44a1ebd2f447b456f824933ebdb1c717feb
Brian Van Buren <brian@spacebrews.com>
2013-04-18 21:42:13 -0400

    Add getter for units of measurement dropdowns

--- protected/components/SbLib.php
+++ protected/components/SbLib.php
@@ -20,4 +20,15 @@ class SbLib extends CComponent
 		}
 		return $string;	
 	}
+	
+	/**
+	 * @return array of different units of measurement
+	 */
+	static function getUnitsOfMeasurement(){
+		return array(
+			'oz'=>'oz',
+			'lbs'=>'lbs',
+			'pack'=>'pack',
+		);
+	}
 }
