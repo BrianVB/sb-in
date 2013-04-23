b77f9843797188dd420aade8288c2e91b6af9d6b
Brian Van Buren <brian@spacebrews.com>
2013-04-22 22:02:05 -0400

    JS to just remove html when a line item is removed

--- protected/views/transaction/create.php
+++ protected/views/transaction/create.php
@@ -14,4 +14,18 @@ $this->menu=array(
 
 <h1>Create Transaction</h1>
 
-<?php echo $this->renderPartial('_form', array('transaction'=>$transaction)); ?>
+<?php echo $this->renderPartial('_form', array(
+	'transaction'=>$transaction,
+	'line_items'=>$line_items
+)); ?>
+
+<?php
+$line_item_index = count($line_items);
+Yii::app()->clientScript->registerScript(
+	'line-item-remove-html',
+	'var line_item_index = '.$line_item_index.';
+	$("body").on("click", ".line-item .remove", function(){
+		$(this).closest("tr").remove();
+	});
+');
+?>
